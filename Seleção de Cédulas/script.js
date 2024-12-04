6c7f779ba1ae40c6a4f472d5b1450205477d9bbe
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-04 10:27:15 -0300

    script.js

--- Seleção de Cédulas/script.js
+++ Seleção de Cédulas/script.js
@@ -1,4 +1,5 @@
 const linksCedulas = {
+    "1 Real": "https://filatelicavitoriaregia.com.br/wp-content/uploads/2023/12/cedula-1-real-2001-anverso.jpg",
     "2 Reais": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSs2Q34ho0mdpyG6uP9Kues39iDHTo-ynSRA&s",
     "5 Reais": "https://www.bcb.gov.br/novasnotas/assets/img/section/5/5_front.jpg",
     "10 Reais": "https://www.bcb.gov.br/novasnotas/assets/img/section/10/10_front.jpg",
@@ -24,7 +25,7 @@ exibirBtn.addEventListener("click", () => {
     const imageUrl = linksCedulas[selectedCedula];
     if (imageUrl) {
         imagemCedula.src = imageUrl;
-        imagemCedula.alt = `Imagem da cédula de ${selectedCedula}`;
+        imagemCedula.alt = `Imagem da cÃ©dula de ${selectedCedula}`;
         imagemCedula.style.display = "block";
         infoText.textContent = `Voce selecionou a cedula de ${selectedCedula}.`;
     } else {
